8f99610cc1a52063f4a55ac0612bce0d04ae470d
PaolaP <paolaperez8@gmail.com>
2022-05-15 11:07:46 -0400

    add navigation component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+
+export default function Navigation(){ return(
+  <Navbar bg="light" expand="lg" className="sticky-top">
+  <Container>
+    <Navbar.Brand href="#home">FunAI</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <NavDropdown title="Prompts" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Tagline</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Poem</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Short Story</NavDropdown.Item>
+          <NavDropdown.Divider />
+          
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+)
+
+}
