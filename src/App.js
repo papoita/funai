1d8a9a8ccb5e6c1c41c2f1eb4c8bacc663f42661
PaolaP <paolaperez8@gmail.com>
2022-05-16 18:33:56 -0400

    add loading state, pass props

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Navigation from "./components/Navigation";
 import PromptForm from "./components/PromptForm";
 import PromptList from "./components/PromptList";
 
+
 function App() {
   const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false)
 
   return (
     <>
       <Navigation />
       <main className="d-flex flex-column align-items-center my-5">
-        <PromptForm setResults={setResults} />
-        <PromptList results={results} />
+        <PromptForm setResults={setResults} setLoading={setLoading} loading={loading} />
+        <PromptList results={results} loading={loading} />
       </main>
     </>
   );
