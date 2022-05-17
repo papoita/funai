3331b5081817f9691d4a4de955d96a18ff2aa915
PaolaP <paolaperez8@gmail.com>
2022-05-17 18:21:56 -0400

    rendering of components unit testing

--- src/components/_tests_/PromptForm.test.js
+++ src/components/_tests_/PromptForm.test.js
@@ -1,20 +1,5 @@
 import React from "react";
-import axios from "axios";
-
-import {
-  render,
-  cleanup,
-  waitForElement,
-  fireEvent,
-  prettyDOM,
-  queryByAltText,
-  queryByText,
-  getByText,
-  getAllByTestId,
-  getByAltText,
-  getByPlaceholderText,
-  getByDisplayValue,
-} from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 
 import PromptForm from "../PromptForm";
 
@@ -25,38 +10,7 @@ describe("PromptForm", () => {
     render(<PromptForm />);
   });
 
-  xit("renders the submit button", () => {
-    render(<PromptForm />);
-  });
-
- xit("defaults input in textarea to predetermined text when no option in the dropdown is selected", () => {
-    const { getByText } = render(<PromptForm />);
-
-    return waitForElement(() =>
-      getByText("Write a thing about any topic")
-    ).then(() => {
-      fireEvent.click(getByText("Write a thing about any topic"));
-      expect(getByText("Write a thing about any topic")).toBeInTheDocument();
-    });
-  });
-
-  xit("text in textarea changes to sentence selected with dropdown word selection in thing option", () => {
-    // ...
-  });
-
-  xit("text in textarea changes to sentence selected with dropdown word selection in topic option", () => {
-    // ...
-  });
-
-  xit("when submit button is clicked axios post event is fired", () => {
-    // ...
-  });
-
-  xit("renders the reset button", () => {
-    render(<PromptForm />);
-  });
-
-  xit("when reset btn is clicked the textarea gets errased/emptied", () => {
+  it("renders the submit button", () => {
     render(<PromptForm />);
   });
 });
--- src/components/_tests_/PromptList.test.js
+++ src/components/_tests_/PromptList.test.js
@@ -4,16 +4,6 @@ import React from "react";
 import {
   render,
   cleanup,
-  waitForElement,
-  fireEvent,
-  prettyDOM,
-  queryByAltText,
-  queryByText,
-  getByText,
-  getAllByTestId,
-  getByAltText,
-  getByPlaceholderText,
-  getByDisplayValue,
 } from "@testing-library/react";
 
 import PromptList from "../PromptForm";
@@ -24,19 +14,6 @@ describe("PromptList", () => {
 it("renders without crashing", () => {
   render(<PromptList />);
 });
-
-it("after submit btn is clicked triggers a loading giff while waiting for axios response", () => {
-  render(<PromptList />);
-});
-
-xit("after submit btn is clicked renders the axios response", () => {
-  render(<PromptList />);
-});
-
-xit("after submit btn is clicked for a second time renders another article with the new response", () => {
-  render(<PromptList />);
-});
-
 
 
 });
