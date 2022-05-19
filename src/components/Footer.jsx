9b57684e4b2dd22fa151ebfec2887cec55f19c3f
PaolaP <paolaperez8@gmail.com>
2022-05-19 17:26:51 -0400

    clean up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,18 @@
 import React from "react";
-import { Card } from "react-bootstrap";
 import { FaGithub, FaCoffee } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <Card bg="primary" variant="light" className=" fixed-bottom mt-5">
-      <Card.Footer className="d-flex text-muted justify-content-center ">
-        <a href={"https://github.com/papoita/funai"}>
-          <FaGithub style={{ color: "#96BF45", margin: "0 20px" }} />
-        </a>
-        <div style={{ margin: "0 20px" }}>
-          Want to chat about awesome ideas? let's connect
-        </div>
-        <a href="https://www.linkedin.com/in/perezleivapaola/">
-          <FaCoffee style={{ color: "#96BF45", margin: "0 20px" }} />
-        </a>
-      </Card.Footer>
-    </Card>
+    <div className="w-100 bg-primary d-flex justify-content-center fixed-bottom p-2 ">
+      <a href="https://github.com/papoita/funai">
+        <FaGithub className="text-secondary mx-3" />
+      </a>
+      <p className="mx-3 text-light">
+        Want to chat about awesome ideas? let's connect
+      </p>
+      <a href="https://www.linkedin.com/in/perezleivapaola/">
+        <FaCoffee className="text-secondary mx-3" />
+      </a>
+    </div>
   );
 }
